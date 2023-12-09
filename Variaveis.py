43179ead0d56e886174e717050b3927a426361b5
Guilherme de Medeiros <guilhe1811@gmail.com>
2023-12-09 18:25:15 -0300

    Variáveis

---
+++ Variaveis.py
@@ -0,0 +1,11 @@
+nome=input('Digite um nome: ')
+idade=int(input('Digite sua idade: '))
+nota_matematica=float(input('Digite a nota da prova de matemática: '))
+nota_fisica=float(input('Digite a nota da prova de física: '))
+nota_lportuguesa = float(input('Digite a nota da prova de português: '))
+
+soma_notas = nota_matematica + nota_fisica + nota_lportuguesa
+
+calcula_media = soma_notas / 3 #Quantidade de matérias
+
+print("Olá, ",nome,'! ',"sua média final foi de ",calcula_media)
