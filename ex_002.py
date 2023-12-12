948c82a309c94e30dc2833dfc5ff296ec3163060
Guilherme de Medeiros <guilhe1811@gmail.com>
2023-12-12 11:42:33 -0300

    Cálculo de média entre duas notas

---
+++ ex_002.py
@@ -0,0 +1,8 @@
+## Proposta: coletar duas notas e calcular sua média
+
+num_1 = int(input("Digite sua primeira nota: "))
+num_2 = int(input("Digite sua segunda nota: "))
+
+media = (num_1 + num_2) / 2
+
+print(f"A média entre {num_1} e {num_2} é {media}")
